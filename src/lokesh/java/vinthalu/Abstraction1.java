5b4cfa088af6c3e1cecc56d6e7d1d6fcb56392f4
lokesh-palieni <lokesh951555@gmail.com>
2023-04-30 09:06:47 +0530

    main method

--- src/lokesh/java/vinthalu/Abstraction1.java
+++ src/lokesh/java/vinthalu/Abstraction1.java
@@ -1,14 +1,19 @@
 package lokesh.java.vinthalu;
 
-abstract class Example{
-	 abstract void m1();
+abstract class Example{       //rule 1:declare as abstract
+	 abstract void m1();       //rule 2:declare as abstract
 	}
  class Sample extends Example{
-	 void m1() {
+	 void m1() {             // rule 3:must implement in sub class
 		 System.out.println("m1 is executed from class sample");
 	 }
  }
 public class Abstraction1 {
-	 
+	 public static void main(String arg[]) {
+		 Example e1;
+		 // e1=new Example(); error    //rule 4:we can't instantiate
+		 e1=new Sample();
+		 e1.m1();
+	 }
 
 }
