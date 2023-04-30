fd47f3cd09d870928fa5e900f2ae27989798f65f
lokesh-palieni <lokesh951555@gmail.com>
2023-04-30 09:14:42 +0530

    examples

---
+++ src/lokesh/java/vinthalu/Abstraction/Abstraction1.java
@@ -0,0 +1,21 @@
+package lokesh.java.vinthalu.Abstraction;
+
+abstract class Example { // rule 1:declare as abstract
+	abstract void m1(); // rule 2:declare as abstract
+}
+
+class Sample extends Example {
+	void m1() { // rule 3:must implement in sub class
+		System.out.println("m1 is executed from class sample");
+	}
+}
+
+public class Abstraction1 {
+	public static void main(String arg[]) {
+		Example e1;
+		// e1=new Example(); error //rule 4:we can't instantiate
+		e1 = new Sample();
+		e1.m1();
+	}
+
+}
---
+++ src/lokesh/java/vinthalu/Abstraction/MultipleabstrctMethods.java
@@ -0,0 +1,36 @@
+package lokesh.java.vinthalu.Abstraction;
+
+abstract class Example1{
+	abstract void m1();
+	abstract void m2();
+}
+abstract class Sample1 extends Example1{
+	void m1() {
+		System.out.println("m1 is executed from class Sample");
+	}
+}
+class Xyz extends Sample1{
+	void m2() {
+		System.out.println("m2 method executed from xyz");
+	}
+}
+class Pqr extends Xyz{
+	void m2() {
+		System.out.println("m1 is executed from class pqr");
+	}
+}
+public class MultipleabstrctMethods {
+
+	public static void main(String[] args) {
+     Example1 e,e2;
+    // e=new Example1;
+    // e=new Sample1;
+     e=new Xyz();
+     e2=new Pqr();
+     e.m1();
+     e.m2();
+     e2.m1();
+     e2.m2();
+	}
+
+}
