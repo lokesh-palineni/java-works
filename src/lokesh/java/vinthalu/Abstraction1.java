d609e04ecac6cf4bcaaa7c79d0ead8ae2afd6cfe
lokesh-palieni <lokesh951555@gmail.com>
2023-04-30 08:32:37 +0530

    concrete cla

--- src/lokesh/java/vinthalu/Abstraction1.java
+++ src/lokesh/java/vinthalu/Abstraction1.java
@@ -3,7 +3,11 @@ package lokesh.java.vinthalu;
 abstract class Example{
 	 abstract void m1();
 	}
-
+ class Sample extends Example{
+	 void m1() {
+		 System.out.println("m1 is executed from class sample");
+	 }
+ }
 public class Abstraction1 {
 	 
 
